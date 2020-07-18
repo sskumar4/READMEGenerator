e9ea6f6f6bc9ece364c7f8acab0740ca4bce31b6
Sudha SK <sskumar4@ncsu.edu>
2020-07-18 16:39:16 -0400

    updated index.js

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 
-
-# q 
-
-
-
-
-
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) 
+# Code Quiz 
 
 * [Description](#Description)
 
@@ -20,37 +15,20 @@
 * [Tests](#Tests)
 
 * [Questions](#Questions)
-
 ## Description
-
-w
-
+A timed quiz app using JavaScript that stores high scores. Code uiz consists of multiple-choice questions with color change and sound feature for right and wrong answer. The app will run in the browser and feature dynamically updated HTML and CSS powered by JavaScript code. It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
 ## Installation
-
-e
-
+No installation needed. Clone the application repositary and run locally using a browser or deploy in a website and view in the browser.
 ## Instructions
-
-r
-
+User is presented with a start quiz button to begin the quiz. A timer starts with total 75 secs in the top right of the app and immediately the user is presented with a question. When one question is answered, the user is presented with another question (Upto seven questions). A "Right" or "Wrong" message is displayed below the multiple choice questions when a user selects an answer. When the answer is correct, a sound is heard and the button color turns green. When the answer is incorrect, a sound is heard and the button color turns red. Also 10 seconds is subtracted from the timer for each incorrect answer. The quiz ends When all questions are answered or the timer reaches 0. When the quiz is over, the user can save their initials and the time score. The user can start a new quiz by clicking "Go Back" button.
 ## License 
-
-This project is licensed under the t 
-
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) License.  
 ## Contribute
-
 Please use my e-mail to contact me on how to contribute to this project.
-
 ## Tests
-
-u
-
+No automated tests written for this app. Launch the app and answer the questions, a README.md is generated at the end.
 ## Questions
-
-sskumar4
+https://github.com/sskumar4
 
 
 sudha1k@yahoo.com
-
-
-
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 //const axios = require("axios");
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // array of questions for user
 const questions = [
@@ -9,46 +9,40 @@ const questions = [
     {
         type: "input",
         message: "What is your Project Title?",
-        name: "projectTitle"
+        name: "projectTitle",
     },
 
     {
         type: "input",
         message: "Provide detail description",
-        name: "projectDescription"
+        name: "projectDescription",
     },
 
     {
         type: "input",
         message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
-        name: "installationInstruction"
-
+        name: "installationInstruction",
     },
     {
         type: "input",
         message: "Provide instructions for use.",
-        name: "usageInstruction"
+        name: "usageInstruction",
     },
     {
-        type: 'list-input',
-        name: 'license',
-        message: 'Select a License ',
-        choices: ['MIT',
-            'Apache 2.0',
-            'BSD',
-            'GPLv3',
-            'ISC'
-        ]
+        type: "list",
+        name: "license",
+        message: "Select a License ",
+        choices: ["MIT", "Apache 2.0", "BSD-3", "GPLv3", "ISC"],
     },
     {
         type: "input",
         message: "Provide guidelines on how to contribute to this project.",
-        name: "contributingInstruction"
+        name: "contributingInstruction",
     },
     {
         type: "input",
         message: "Provide examples on how to run tests.",
-        name: "tests"
+        name: "tests",
     },
 
     // {
@@ -59,109 +53,117 @@ const questions = [
     {
         type: "input",
         message: "What is your GitHub user name?",
-        name: "gitHubUsername"
+        name: "gitHubUsername",
     },
     {
         type: "input",
         message: "What is your E-mail?",
-        name: "email"
+        name: "email",
     },
-
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    var writeResult = fs.writeFileSync(path.join(__dirname, '../ReadMeGenerator', fileName), data);
+    var writeResult = fs.writeFileSync(
+        path.join(__dirname, "../ReadMeGenerator", fileName),
+        data
+    );
 
-    console.log("file generated....")
+    console.log("file generated....");
 }
 
 // function to initialize program
 function init() {
     console.log(`Begin`);
-    const userResponse = inquirer
-        .prompt(questions).then(function(userResponse) {
+    const userResponse = inquirer.prompt(questions).then(function(userResponse) {
+        console.log(`starting`);
+        console.log(userResponse);
+        const gitUsername = userResponse.gitHubUsername;
+        const projectTitle = userResponse.projectTitle;
+        const projectDescription = userResponse.projectDescription;
 
-            console.log(`starting`);
-            console.log(userResponse);
-            const gitUsername = userResponse.gitHubUsername;
-            const projectTitle = userResponse.projectTitle;
-            const projectDescription = userResponse.projectDescription;
+        const installationProcess = userResponse.installationInstruction;
 
-            const installationProcess = userResponse.installationInstruction;
+        const instruction = userResponse.usageInstruction;
 
-            const instruction = userResponse.usageInstruction;
+        // const instructionExample = userResponse.instructionExample;
 
-            // const instructionExample = userResponse.instructionExample;
+        const licenseName = userResponse.license;
+        let licenseLogo = "";
+        let licenseText = "";
+        switch (licenseName) {
+            case "MIT":
+                licenseLogo = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) `;
+                licenseText = `[MIT](https://opensource.org/licenses/MIT) License. `;
+                break;
+            case "Apache 2.0":
+                console.log('Came here at Apache');
+                licenseLogo = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
 
-            const licenseName = userResponse.license;
+                licenseText = `[Apache-2.0](https://opensource.org/licenses/Apache-2.0) License. `;
+                break;
+            case "BSD-3":
+                console.log('Came here at BSD-3');
+                licenseLog = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+                licenseText = `[BSD-3-Clause](https://opensource.org/licenses/BSD-3-Clause) License. `;
+                break;
 
-            // const licenseUrl = userResponse.licenseUrl;
+            case "GPLv3":
+                licenseLogo = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+                licenseText = `[gpl-3.0](https://opensource.org/licenses/gpl-3.0) License. `;
+                break;
 
-            const Contributing = "Please use my e-mail to contact me on how to contribute to this project.";
+            case "ISC":
+                licenseLogo = `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+                licenseText = `[ISC](https://opensource.org/licenses/ISC) License. `;
+                break;
+            default:
+                break;
+        }
+        console.log(licenseLogo);
+        // const licenseUrl = userResponse.licenseUrl;
 
-            const tests = userResponse.tests;
+        const Contributing =
+            "Please use my e-mail to contact me on how to contribute to this project.";
 
-            const gitHubUserName = userResponse.gitHubUsername;
+        const tests = userResponse.tests;
 
-            const email = userResponse.email;
+        const gitHubUserName = userResponse.gitHubUsername;
 
-            // // fetching data from git
+        const email = userResponse.email;
 
-            // user
-            /*
-                        const gitResponse = await axios.get(`https://api.github.com/users/${gitUsername}`);
+        // // fetching data from git
 
-                        const gitData = gitResponse.data;
-
-                        const gitName = gitData.login;
-
-                        const gitEmail = gitData.email;
-
-                        const gitlocation = gitData.location;
-
-                        const gitUrl = gitData.html_url;
-
-                        const gitProfileImage = gitData.avatar_url;
-            */
-            // contributor
-            /*
-                const contributorUserNamesArray = contributorUserNames.split(",");
-
-                console.log(contributorUserNamesArray);
-
-                // const  = listOfContributorsUserNames.
-
-                // contributorsGitUserName
-
-                var resultContributor;
-
-                for (i = 0; i < contributorUserNamesArray.length; i++) {
-
-                    var contributorsGitUserName = contributorUserNamesArray[i]
-
-                    const gitResponse2 = await axios.get(`https://api.github.com/users/${contributorsGitUserName}`);
-
-                    var gitContribuProfileImage = gitResponse2.data.avatar_url;
-
-                    var gitContribuUrl = gitResponse2.data.html_url;
-
-                    var gitContribuEmail = gitResponse2.data.email;
-
-                    var resultContributor = resultContributor + (`
-
-                        \n <img src="${gitContribuProfileImage}" alt="drawing" width="150" display="inline"/> ${contributorsGitUserName}  GitHubLink: ${gitContribuUrl}`);
-
-                }
-            */
-            var result = (`
-
+        // user
+        /*
+                            const gitResponse = await axios.get(`https://api.github.com/users/${gitUsername}`);
+                            const gitData = gitResponse.data;
+                            const gitName = gitData.login;
+                            const gitEmail = gitData.email;
+                            const gitlocation = gitData.location;
+                            const gitUrl = gitData.html_url;
+                            const gitProfileImage = gitData.avatar_url;
+                */
+        // contributor
+        /*
+                    const contributorUserNamesArray = contributorUserNames.split(",");
+                    console.log(contributorUserNamesArray);
+                    // const  = listOfContributorsUserNames.
+                    // contributorsGitUserName
+                    var resultContributor;
+                    for (i = 0; i < contributorUserNamesArray.length; i++) {
+                        var contributorsGitUserName = contributorUserNamesArray[i]
+                        const gitResponse2 = await axios.get(`https://api.github.com/users/${contributorsGitUserName}`);
+                        var gitContribuProfileImage = gitResponse2.data.avatar_url;
+                        var gitContribuUrl = gitResponse2.data.html_url;
+                        var gitContribuEmail = gitResponse2.data.email;
+                        var resultContributor = resultContributor + (`
+                            \n <img src="${gitContribuProfileImage}" alt="drawing" width="150" display="inline"/> ${contributorsGitUserName}  GitHubLink: ${gitContribuUrl}`);
+                    }
+                */
+        var result = `
+${licenseLogo}
 # ${projectTitle} 
-
-
-
-
-
 \n* [Description](#Description)
 \n* [Installation](#Installation)
 \n* [Instructions](#Instructions)
@@ -169,44 +171,26 @@ function init() {
 \n* [Contribute](#Contribute)
 \n* [Tests](#Tests)
 \n* [Questions](#Questions)
-
 ## Description
-
 ${projectDescription}
-
 ## Installation
-
 ${installationProcess}
-
 ## Instructions
-
 ${instruction}
-
 ## License 
-
-This project is licensed under the ${licenseName} 
-
+This project is licensed under the ${licenseText} 
 ## Contribute
-
 ${Contributing}
-
 ## Tests
-
 ${tests}
-
 ## Questions
-
-${gitHubUserName}
+https://github.com/${gitHubUserName}
 \n
 ${email}
-
-
-
-`)
-            writeToFile('README.md', result);
-        });
+`;
+        writeToFile("README.md", result);
+    });
 }
-
 
 // function call to initialize program
 init();
